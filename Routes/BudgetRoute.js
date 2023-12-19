838b6a91542d3f282920fa531d2561be151c07e4
Sanju P Saji <115170042+SanjuPSaji@users.noreply.github.com>
2023-12-20 01:29:51 +0530

    Delete Routes directory

--- Routes/BudgetRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const BudgetController = require('../Controller/BudgetController')
-
-router.post('/createBudget', BudgetController.createBudget)
-router.post('/fetchBudget', BudgetController.fetchBudget)
-router.post('/deleteBudget' , BudgetController.deleteBudget)
-module.exports = router
